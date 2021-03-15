9fcd810abdf4e8787270ae233d6eeb044c97ddb0
Lubna Shaikh <67864952+lubnahub@users.noreply.github.com>
2021-03-15 12:01:06 -0700

    Update end.js

--- end.js
+++ end.js
@@ -2,12 +2,42 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
+var text2=document.getElementById("id001");
 
 finalScore.innerHTML = mostRecentScore;
+if(finalScore.innerHTML==20)
+{
+    text2.innerHTML="Congratulations! That was really bad.";
+}
+else if(finalScore.innerHTML<=40)
+{
+    text2.innerHTML="Congratulations! That was bad.";
+}
+else if(finalScore.innerHTML<=60)
+{
+    text2.innerHTML="Congratulations! You'll probably do better in the next attempt.";
+}
+else if(finalScore.innerHTML<=80)
+{
+    text2.innerHTML="Congratulations! There is definitely room for improvement.";
+}
+else if(finalScore.innerHTML<=100)
+{
+    text2.innerHTML="Congratulations! That's a decent score.";
+}
+else if(finalScore.innerHTML<=120)
+{
+    text2.innerHTML="Congratulations! That was awesome.";
+}
+else if(finalScore.innerHTML<=140)
+{
+    text2.innerHTML="Congratulations! What a nerd!";
+}
+
+
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
